772e6f60104a228fb1c59e3e6196a30f4b09e3db
DanteBLima <160533090+DanteBLima@users.noreply.github.com>
2024-11-19 20:57:29 -0300

    Add files via upload

---
+++ load.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const menuToggle = document.querySelector('.menu-toggle');
+        const navUl = document.querySelector('nav ul');
+
+        menuToggle.addEventListener('click', () => {
+            navUl.classList.toggle('show');
+        });
+    
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    
+});
